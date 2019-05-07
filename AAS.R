303e4c3fd9c251ec0721e7dc76f776e56936ad56
kinleyfanjl <kinleyfanjl@users.noreply.github.com>
2019-05-07 10:16:45 +0800

    Update AAS.R

--- AAS.R
+++ AAS.R
@@ -1,16 +1,26 @@
+cond = read_excel('admited_condi.xlsx')
+cond_cities = cond$城市
+cond_years = cond$车龄
+cond_dist = cond$行驶里程
+cond_insur = cond$保险
 
-library(jiebaR)
-library(readxl)
-library('tm')
-library('NLP')
-library('cidian')
-library('stingi')
-library('pbapply')
-library(stringr)
-library(readxl)
-library(dplyr)
-library(ggplot2)
-library(maps)
-library(mapdata)
-library(maptools)
+cond_data = data.frame(cond_cities)
+cond_dist = sapply(cond_dist, FUN = str_extract, 
+                   pattern = "\\d+")
+cond_data$dist = cond_dist
+cond_data$dist[which(is.na(cond_data$dist))] = 0  
+cond_data$insur = sapply(cond_insur, function(x){sum(
+  strsplit(x, split = '')[[1]] == '险')})
+cond_data$insur[which(is.na(cond_data$insur))] = 0 
+cond_data$years = sapply(cond_years, FUN = str_extract, 
+                   pattern = "\\d+")
+cond_data$years[which(is.na(cond_data$years))] = 0  
+cond_data$years[60] = 3
+cond_data %>% filter(cond_cities %in% city_plt)
+cond_data1 = cond_data %>% filter(cond_cities %in% city_plt)
+cond_data1 = cond_data1[!duplicated(cond_data1$cond_cities),]
 
+
+final_data1 = merge(cond_data1, final_data, by.x = 'cond_cities', by.y = 'cities')
+final_data2 = merge(final_data1, news_plt_data, by.x = 'cond_cities', by.y = 'cities')
+View(final_data2)
