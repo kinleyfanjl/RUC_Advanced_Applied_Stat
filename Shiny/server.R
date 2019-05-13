cf611412f287b94f99c4f04f3eef0fb19e3c7489
kinleyfanjl <kinleyfanjl@users.noreply.github.com>
2019-05-13 13:59:37 +0800

    Update server.R

--- Shiny/server.R
+++ Shiny/server.R
@@ -1,6 +1,7 @@
 ## server.R
 load('final_data.Rdata')
 load('map.Rdata')
+load('d')
 library(dplyr)
 library(readxl)
 library(dplyr)
@@ -8,42 +9,63 @@ library(ggplot2)
 library(maps)
 library(mapdata)
 library(maptools)
-
+library(ggmap)
 library(shiny)
+library(wordcloud)
 
 shinyServer(function(input, output) {
     maptypeInput = eventReactive(input$update, {
       switch(input$type,
              'map' = 'map',
-             'curve' = 'curve')
+             'curve' = 'curve',
+             'words' = 'words')
     })
+   
   # Generate a summary of the dataset ----
     output$view <- renderTable({
-      head(final_data, n = isolate(input$obs))
+      # if(maptypeInput() == 'map')
+      #   {
+      # final_data[sample(1:nrow(final_data), isolate(input$num)),]
+      # }
+      # if(maptypeInput() == 'cu')
+      # {
+      #   final_data[sample(1:nrow(final_data), isolate(input$num)),]
+      # }
+      if(maptypeInput() == 'words'){
+        d[sample(1:nrow(d), isolate(input$num)),]
+      }
+      else{
+        final_data[sample(1:nrow(final_data), isolate(input$num)),]
+      }
+      
     })
     
   
   #####plot about entropy and Monop.######
+
   output$map <- renderPlot({
-    K = input$top
+    K = isolate(input$top)
     final_data_plt_cities = final_data %>% filter(rank(entropy) > length(entropy) - K) 
-    
-      if(maptypeInput() == 'map'){
-        mymap = ggplot(data = fortify(map)) +
-          geom_polygon(aes(x = long, y = lat, group=group), colour = "gray",fill = "aliceblue") +
-          labs(title="The number of admitted licenses for E-Taxi companies",x="",y="") +
-          geom_point(data=final_data_plt_cities,aes(x=lon,y=lat,colour=entropy,size = No.lice, pch = fac_total ),alpha = 0.8)+ 
-          scale_color_gradientn(colours = rainbow(5)) 
-        mymap
-      }
-    if(maptypeInput() == 'curve'){
+    if(maptypeInput() == 'map'){
+      plot(map)
+      points(final_data_plt_cities$lon, final_data_plt_cities$lat, pch = 20, col = 'blue')
+    }
+    else{ 
+      if(maptypeInput() == 'curve'){
       p = ggplot(final_data,aes(x=total,y = entropy))
-      p = p + geom_point(data = final_data, alpha = 0.7, aes(colour = No.lice)) + geom_smooth(lwd = 0.6,col = 'red')+
+      p = p + geom_point(data = final_data_plt_cities, alpha = 0.7, aes(colour = No.lice)) + geom_smooth(lwd = 0.6,col = 'red')+
         labs(title = 'Total Taxis, Monopolization, # of admitted lincenses') +
         scale_color_gradient(low="blue", high="red")
       p
+      }
+      else{
+        if(maptypeInput() == 'words'){
+          wordcloud(d[1:K,1], d[1:K,2], scale = c(4,0.5),
+                         family = 'STKaiti', colors=brewer.pal(8, "Dark2"))
+          
+        }
+      }
     }
-    
   })
   
 })
